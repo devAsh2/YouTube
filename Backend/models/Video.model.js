4273eea084ee016510bfb957207eefde52a98657
devAsh2 <ashishudupa282000@gmail.com>
2026-04-02 18:01:10 +0530

    Created video model

---
+++ Backend/models/Video.model.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+			trim: true,
+			maxlength: 200,
+		},
+		thumbnailUrl: {
+			type: String,
+			required: true,
+		},
+		videoUrl: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			trim: true,
+			maxlength: 5000,
+		},
+		channelId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Channel",
+			required: true,
+		},
+		uploader: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		views: {
+			type: Number,
+			default: 0,
+		},
+		likes: {
+			type: Number,
+			default: 0,
+		},
+		dislikes: {
+			type: Number,
+			default: 0,
+		},
+		comments: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Comment",
+				default: [],
+			},
+		],
+		category: {
+			type: String,
+			enum: [
+				"Education",
+				"Entertainment",
+				"Gaming",
+				"Music",
+				"Sports",
+				"News",
+				"Technology",
+				"Other",
+			],
+			default: "Entertainment",
+		},
+	},
+	{
+		timestamps: true,
+	},
+);
+
+export const Video = mongoose.model("Video", videoSchema);
